c2873fc3f3dad1d3cec786182147c556ae085899
Rahul <rahul39khanal@gmail.com>
2023-05-01 11:09:08 +0545

    on

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express()
+const PORT = process.env.PORT || 3000
+app.use(express.static('public'))
+
+const server = app.listen(PORT, () => {
+    console.log("Connected in port.................")
+})
+
+app.get("/", (req, resp) => {
+    resp.sendFile(__dirname + '/index.html')
+})
+let io = require('socket.io')(server)
+io.on('connection', (socket) => {
+    console.log(`socket connected in ${socket.id}`);
+    socket.on('comment', (data)=>{
+        console.log(data);
+        data.time = Date()
+        socket.broadcast.emit('comment2', data)
+    })
+})
---
+++ public/js/index.js
@@ -0,0 +1,54 @@
+let username;
+let socket = io()
+
+do {
+    username = prompt("Enter your Name")
+} while (!username)
+
+const textarea = document.querySelector("#comment-box");
+const btn = document.querySelector("#Submit");
+const comment_stack = document.querySelector("#comment-stack");
+btn.addEventListener('click', () => {
+    let comment = textarea.value;
+    if (!comment) {
+        alert('Empty?')
+        return
+    }
+    postComment(comment)
+})
+
+function postComment(comment) {
+    //Append to dom
+    let data = {
+        username: username,
+        comment: comment
+    }
+    appendToDom(data)
+
+    //Broadcast
+    broadcastComment(data)
+    //sync in mysql
+}
+
+function appendToDom(data) {
+    let ltag = document.createElement('li');
+    let markup = `
+        <h3>${data.username}</h3>
+        <p>${data.comment}</p>
+        <div><b>${new Date().getTime()}</b></div>
+        <br>
+    `
+    ltag.innerHTML = markup
+    comment_stack.prepend(ltag)
+    textarea.value = ""
+}
+
+function broadcastComment(data) {
+    socket.emit('comment', data)
+}
+
+socket.on("comment2", (data) => {
+    console.log("I am connected");
+    console.log(data);
+    appendToDom(data)
+})
